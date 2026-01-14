930bfaa52664b0171e23358f645c1fae5a1957bd
Leonela88 <rivasaela@gmail.com>
2026-01-14 10:24:25 +0100

    feat: Card and Form component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './componentes/card/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Conozca al equipo apasionado</h1>
+      <Card/>
       <div className='carrusel'></div>
       <div className='contacto'></div>
     </>
--- src/componentes/card/Card.jsx
+++ src/componentes/card/Card.jsx
@@ -1,14 +1,22 @@
 
-const Card = () => {
-    return (
+const Card =() => {
 
+return (
 
-<h3> Nat Reynolds</h3>
-<h4>Fundador</h4>
+        <div>
+            <img src="./public/leoPic.jpg" alt="Foto" />
+            <h3> Nat Reynolds</h3>
+            <h4>Fundador</h4>
+            <div className="icons">
+            <img src="#" alt="Facebook" />
+            <img src="#" alt="instagram" />
+            <img src="#" alt="x" />
+            </div>
+            
+        </div>
+
+    
 
     )
-
-
-
 }
-export default Card
+export default Card;
